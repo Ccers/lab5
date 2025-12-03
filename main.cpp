c6c0f1deb3893d067d893d1e824c6224fcc5cec1
Simin Guo <1205609108@qq.com>
2025-12-03 22:54:58 +0800

    Cc修改函数main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-cout << "Hello, World!" << endl; // 输出 Hello, World!
+cout << "A: Hello, World!" << endl; // 输出 Hello, World!
 cout<<"This is in feature-maincpp"<<'\n';
 return 0; // 返回 0 表示程序成功执行
 }
